096719ca553b2e97704d411053cc27451009b0fc
carly <carly@noukod.com>
2019-01-21 20:34:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,9 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-    create_table :posts do |t|
+    create_table :students do |t|
       t.text :first_name
       t.text :last_name
- 
+
       t.timestamps null: false
     end
   end
